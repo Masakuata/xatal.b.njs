999382cbfe455e5a5810aea7137c02b09c837d16
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2023-06-25 14:20:14 -0600

    Using the same mongodb connection instead of opening and closing

--- src/connection/mongoHandler.ts
+++ src/connection/mongoHandler.ts
@@ -25,30 +25,19 @@ export class MongoHandler {
   }
 
   public async findById(id: string) {
-    await MongoHandler.client.connect();
-    const result = await this.collection.findOne(new ObjectId(id));
-    await MongoHandler.client.close();
-    return result;
+    return await this.collection.findOne(new ObjectId(id));
   }
 
   public async findOne(filter: Document) {
-    await MongoHandler.client.connect();
-    const result = await this.collection.findOne(filter);
-    await MongoHandler.client.close();
-    return result;
+    return await this.collection.findOne(filter);
   }
 
   public async findMany(filter: Document) {
-    await MongoHandler.client.connect();
-    const result = this.collection.find(filter);
-    await MongoHandler.client.close();
-    return result;
+    return this.collection.find(filter);
   }
 
   public async insertOne(value: Document): Promise<Document | null> {
-    await MongoHandler.client.connect();
     const result = await this.collection.insertOne(value);
-    await MongoHandler.client.close();
     if (result.insertedId !== null) {
       return value;
     }
@@ -56,49 +45,36 @@ export class MongoHandler {
   }
 
   public async count(filter: Document): Promise<number> {
-    await MongoHandler.client.connect();
-    const count = await this.collection.countDocuments(filter);
-    await MongoHandler.client.close();
-    return count;
+    return await this.collection.countDocuments(filter);
   }
 
   public async updateOne(filter: Document, updatedData: Document) {
-    await MongoHandler.client.connect();
     const result = await this.collection.updateOne(
       filter,
       { $set: { updatedData } },
       { upsert: true });
-    await MongoHandler.client.close();
     return result.upsertedCount > 0;
   }
 
   public async updateMany(filter: Document, updatedData: Document) {
-    await MongoHandler.client.connect();
     const result = await this.collection.updateMany(
       filter,
       { $set: { updatedData } });
-    await MongoHandler.client.close();
     return result.upsertedCount > 0;
   }
 
   public async deleteById(id: string) {
-    await MongoHandler.client.connect();
     const result = await this.collection.deleteOne({ _id: new ObjectId(id) });
-    await MongoHandler.client.close();
     return result.deletedCount > 0;
   }
 
   public async deleteOne(filter: Document) {
-    await MongoHandler.client.connect();
     const result = await this.collection.deleteOne(filter);
-    await MongoHandler.client.close();
     return result.deletedCount > 0;
   }
 
   public async deleteMany(filter: Document) {
-    await MongoHandler.client.connect();
     const result = await this.collection.deleteMany(filter);
-    await MongoHandler.client.close();
     return result.deletedCount > 0;
   }
 }
